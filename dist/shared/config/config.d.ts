09f3725ea09d38cc391459473d48ceeb6d228633
tongtong.ctt <tongtong.ctt@alibaba-inc.com>
2025-07-03 21:21:12 +0800

    feat: remove build dist

--- dist/shared/config/config.d.ts
+++
@@ -1,4 +0,0 @@
-import 'dotenv/config';
-import { Config } from '../types/common';
-declare const config: Config;
-export default config;
--- dist/shared/types/common.js
+++
@@ -1,6 +0,0 @@
-// @ts-nocheck
-// @ts-nocheck
-/**
- * 通用类型定义
- */
-export {};
